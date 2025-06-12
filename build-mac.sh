3cf3f119fbc0e62c672230176789c334a7852c84
guspuffygit <guspuffy@gmail.com>
2025-06-12 14:29:54 -0400

    saving

---
+++ build-mac.sh
@@ -0,0 +1,19 @@
+SDKDIR="$(xcrun --sdk macosx --show-sdk-path)" \
+CPPFLAGS="-I$(brew --prefix gdbm)/include -I$(brew --prefix xz)/include \
+          -I$(brew --prefix zlib)/include -I$(brew --prefix ncurses)/include \
+          -I$(brew --prefix readline)/include" \
+LDFLAGS="-L$(brew --prefix gdbm)/lib -L$(brew --prefix xz)/lib \
+         -L$(brew --prefix zlib)/lib -L$(brew --prefix ncurses)/lib \
+         -L$(brew --prefix readline)/lib" \
+./configure --with-openssl="$(brew --prefix openssl@3)" \
+            --with-tcltk-libs="$(pkg-config --libs tcl tk)" \
+            --with-tcltk-includes="$(pkg-config --cflags tcl tk)" \
+            --with-dbmliborder=gdbm:ndbm \
+            --with-ensurepip=upgrade \
+            --enable-shared \
+            --enable-universalsdk="$(xcrun --sdk macosx --show-sdk-path)" \
+            --with-universal-archs=universal2 \
+            --with-suffix=""
+
+make
+make install
