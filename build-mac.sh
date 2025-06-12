14c62639046a3b593097bd3d8c5503792a2a2f42
guspuffygit <guspuffy@gmail.com>
2025-06-12 12:40:35 -0400

    remove debug

--- build-mac.sh
+++ build-mac.sh
@@ -5,15 +5,14 @@ CPPFLAGS="-I$(brew --prefix gdbm)/include -I$(brew --prefix xz)/include \
 LDFLAGS="-L$(brew --prefix gdbm)/lib -L$(brew --prefix xz)/lib \
          -L$(brew --prefix zlib)/lib -L$(brew --prefix ncurses)/lib \
          -L$(brew --prefix readline)/lib" \
-./configure --with-pydebug \
-            --with-openssl="$(brew --prefix openssl@3)" \
+./configure --with-openssl="$(brew --prefix openssl@3)" \
             --with-tcltk-libs="$(pkg-config --libs tcl tk)" \
             --with-tcltk-includes="$(pkg-config --cflags tcl tk)" \
             --with-dbmliborder=gdbm:ndbm \
             --with-ensurepip=upgrade \
             --enable-shared \
             --enable-universalsdk="$(xcrun --sdk macosx --show-sdk-path)" \
-            --with-universal-archs=intel-64 \
+            --with-universal-archs=all \
             --with-suffix=""
 
 make
